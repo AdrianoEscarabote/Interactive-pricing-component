b42353605e4e279b5bcdd66bab98a5145665bdc4
AdrianoEscarabote <drian.escarabote@hotmail.com>
2022-12-22 18:32:33 -0300

    src

---
+++ src/components/Article.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+export default function Article() {
+  return (
+    <article>
+      <h1>Simple, traffic-based pricing</h1>
+      <p>Sign-up for our 30-day trial. No credit card required.</p>
+    </article>
+  )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from "react";
+import Article from "./Article";
+import { iconCheck } from "../../images/icon-check.svg";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <Article />
+        <h1>oi</h1>
+        <img src={iconCheck} alt="" aria-hidden="true" />   
+      </Fragment>
+    )
+  }
+}
+
+export default Main;
+
+/* return (
+    <Fragment>
+      <div class="pricing-component">
+        <div class="infos">
+          <div class="views">
+            <p><span id="numberView">100k</span> Pageviews</p>
+          </div>
+          <div class="price">
+            <p><span id="number">$ 16.00</span> /month</p>
+          </div>
+        </div>
+
+        <div class="input">
+          <input type="range" name="range" id="range">
+        </div>
+
+        <div class="billing">
+          <p>Monthly Billing</p>
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+              <label class="form-check-label" for="flexSwitchCheckDefault"></label>
+          </div>
+          <p>Yearly Billing <span class="discount">25% discount</span></p>
+        </div>
+
+        <div class="details">
+          <ul>
+            <li><img src={iconCheck} alt="" aria-hidden="true">
+              <p>Unlimited websites</p>
+            </li>
+            <li><img src={iconCheck} alt="" aria-hidden="true">
+              <p>100% data ownership</p>
+            </li>
+            <li><img src={iconCheck} alt="" aria-hidden="true">
+              <p>Email reports</p>
+            </li>
+          </ul>
+          <button>Start my trial</button>
+        </div>
+      </div>
+    </Fragment>
+  )*/
